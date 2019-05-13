0b056d988b5e3df1a1dca8719587beb0b5fde194
LBeast13 <liam.bette@hotmail.com>
2019-05-13 11:41:02 +0200

    Check inputs cloture

--- PROACTIF_SHN/src/main/webapp/JS/clotureInterventionEmploye.js
+++ PROACTIF_SHN/src/main/webapp/JS/clotureInterventionEmploye.js
@@ -48,8 +48,68 @@ function cloturerIntervention(){
 }
 
 function checkChampsValides(){
-    return false;
+    statut = $("#inputStatut").val();
+    date = $("#inputDateCloture").val();
+    commentaire = $("#inputCommentaire").val();
+    
+    if(statut === "" || date === "" | commentaire === ""){
+        return false;
+    } else if(statut !== "Echec" || statut !== "Succès"){
+        return false;
+    } else if(!isValidDate(date)){
+        return false;
+    }
+    
+    return true;
 }
+
+/**
+ * Validation du format de la date entrée sous la forme JJ/MM/AAAA
+ * @param {type} dateString
+ * @returns {Boolean} */
+function isValidDate(dateString)
+{
+    // Première vérification du format
+    if(!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dateString))
+        return false;
+
+    // Conversion des parties de la date sous forme d'entiers
+    var parts = dateString.split("/");
+    var day = parseInt(parts[0], 10);
+    var month = parseInt(parts[1], 10);
+    var year = parseInt(parts[2], 10);
+    
+    // Conversion des parties de la date de demande
+    var partsDemande = $("#dateDemande").val().split("/");
+    var dayDemande = parseInt(partsDemande[0], 10);
+    var monthDemande = parseInt(partsDemande[1], 10);
+    var yearDemande = parseInt(partsDemande[2], 10);
+
+    // Vérification de l'année, du mois et du jour
+    if(year < yearDemande || year > 3000 || month === 0 || month > 12){
+        
+        // Cas mois < mois demande
+        if(year === yearDemande 
+           && month < monthDemande){
+            return false;
+        }
+        // Cas jour < jour demande
+        else if(year === yearDemande 
+                && month === monthDemande
+                && day < dayDemande){
+            return false;
+        }
+    }
+
+    var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+
+    // Ajustement pour les années bisextiles
+    if(year%400 === 0 || (year%100 !== 0 && year%4 === 0))
+        monthLength[1] = 29;
+
+    // On vérifie le jour du mois
+    return day > 0 && day <= monthLength[month - 1];
+};
 
 $(document).ready(function () {
     console.log("TODO : gérer les affichages des cases en fonction du type d'intervention");
