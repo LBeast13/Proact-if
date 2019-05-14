101a44f3b8ff8c592b23f54f69f6dbb9e231de3a
Thibault Remy <tremy@insa-lyon.fr>
2019-05-14 17:10:16 +0200

    un debut de client

---
+++ PROACTIF_SHN/src/main/java/action/client/RecupererDetailInterventionClientAction.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package action.client;
+
+import action.Action;
+import action.Action;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import metier.modele.Client;
+import metier.modele.Intervention;
+import metier.modele.Intervention_Animal;
+import metier.modele.Intervention_Livraison;
+import metier.service.Service;
+/**
+ *
+ * @author tremy
+ */
+public class RecupererDetailInterventionClientAction extends Action {
+   
+ @Override
+    public boolean executer(HttpServletRequest request, HttpSession session) {
+      
+        System.out.println((String)request.getAttribute("numero_interv"));
+        // Récupérationdu numéro d'intervention
+        Integer numeroInterv = Integer.parseInt((String)request.getParameter("numero_interv"));
+    
+        Intervention interv = Service.getIntervention(numeroInterv);
+
+        request.setAttribute("numero_interv", interv.getNumIntervention().toString());
+        request.setAttribute("numero_client", interv.getClient_associe().getIdPersonne().toString());
+        request.setAttribute("numero_employe", interv.getEmploye_associe().getIdPersonne().toString());
+        request.setAttribute("type_interv", interv.getType());
+        request.setAttribute("date_demande", formattageDate(interv.getDateDebut()));
+        request.setAttribute("statut_interv", getStatut(interv));
+        request.setAttribute("description_interv", interv.getDescription());
+        request.setAttribute("commentaire_interv", interv.getCommentaireFin());
+        
+        switch(interv.getType()){
+            case "Intervention Animal":
+                Intervention_Animal intervAnimal = (Intervention_Animal) interv;
+                request.setAttribute("animal_interv", intervAnimal.getAnimal());
+                //System.out.println(intervAnimal.getAnimal());
+                break;
+                
+            case "Intervention Livraison":
+                Intervention_Livraison intervLivraison = (Intervention_Livraison) interv;
+                request.setAttribute("objet_interv", intervLivraison.getObjet());
+                request.setAttribute("entreprise_interv", intervLivraison.getEntreprise());
+                //System.out.println(intervLivraison.getObjet());
+                //System.out.println(intervLivraison.getEntreprise());
+                break;
+        }
+        
+        if(interv.estTerminee()){
+            request.setAttribute("date_cloture", formattageDate(interv.getDaterFin())); 
+        } else{
+            request.setAttribute("date_cloture", "En cours...");
+        }
+
+        return true;
+    }
+    
+    /**
+     * Formate le statut de l'intervention
+     * @param interv l'intervention à formater
+     * @return le statut formaté
+     */
+    private String getStatut(Intervention interv) {
+        if (interv.getDaterFin() != null) {
+            String statut = interv.getStatut().toString();
+            switch (statut) {
+                case "SUCCES":
+                    return "Succès";
+                case "ECHEC":
+                    return "Echec";
+                default:
+                    return "Erreur statut";
+            }
+        } else {
+            return "En cours";
+        }
+    }
+    
+}
+
+
+
+
+   
---
+++ PROACTIF_SHN/src/main/webapp/JS/infoPersoClient.js
@@ -0,0 +1,65 @@
+/**
+ * Fonction Ajax qui met à jours le DOM avec les données personnelles du
+ *client connecté.
+ * @returns {undefined}
+ */
+function remplirDonneesPerso() {
+    $.ajax({
+        url: '../../ActionServlet',
+        method: 'POST',
+        data: {
+            todo: 'remplir_informations_perso',
+        },
+        dataType: 'json'
+    }).done(function (response) { // Appel OK
+        console.log("TODO : Régler le raffraichissement de la page lors de l'appel\n\
+        AJAX de remplir d'info perso (on ne veut pas de raffraichissement pour pas avoir 2 appels)");
+        civilite = response.civilite;
+        updateCivilite(civilite);
+        $('#nomClient').html(response.nom);
+        $('#prenomClient').html(response.prenom);
+        $('#dateNaissanceClient').html(response.dateNaissance);
+        $('#emailClient').html(response.email);
+        $('#telephoneClient').html(response.tel);
+        $('#adresseClient').html(response.adresse);
+        $('#codePostalClient').html(response.codePostal);
+        $('#villeAdresseClient').html(response.ville);
+       
+    }).fail( function (error) { // Appel KO => erreur a gérer
+        console.log("Fail remplir_informations_perso");          
+        // Popup avec message d'erreur :
+        alert('Erreur lors de l\'appel: HTTP Code ' + error.status + ' ~ ' + error.statusText + ' ~ ' + error.getResponseHeader('Content-Type'));
+    });
+}
+
+/**
+ * Met à jour la Tab du DOM pour la civilité
+ * @param {String} civilite du client connecté
+ * @returns {undefined}
+ */
+function updateCivilite(civilite){
+    console.log(civilite);
+    if(civilite === 'M'){
+        $('#ongletMr').prop("disabled", false);
+        $('#ongletMme').prop("disabled", true);
+        $('#ongletMlle').prop("disabled", true);
+    }
+    else if(civilite === 'Mme'){
+        $('#ongletMr').prop("disabled", true);
+        $('#ongletMme').prop("disabled", false);
+        $('#ongletMlle').prop("disabled", true);
+    }
+    else if(civilite === 'Mlle'){
+        $('#ongletMr').prop("disabled", true);
+        $('#ongletMme').prop("disabled", true);
+        $('#ongletMlle').prop("disabled", false);
+    }
+}
+
+
+$(document).ready(function () {
+    remplirDonneesPerso();
+});
+
+
+
